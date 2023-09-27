64a21dc56b085ed0eb1189e02d97d1a768ddf224
JimChen <tc21122402@tzuchi.com.tw>
2023-09-27 10:34:19 +0800

    (dollob) import bootstrap5

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myblog.apps.MyblogConfig"
+    "myblog.apps.MyblogConfig",
+    "bootstrap5"
 ]
 
 MIDDLEWARE = [
